1f1d148282c065ed54e3a73797de93ad10b93c6b
hripsimeghazaryan <43012545+hripsimeghazaryan@users.noreply.github.com>
2018-09-09 13:32:52 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,19 @@
+const a = "Welcome to";
+const b = " Marvel ";
+const c = "Cinematic";
+const d = " Universe ";
+const e = 2022;
+const f = 61
+
+alert ( a + b + c + d );
+alert ( e - f );
+alert ( b + d );
+
+const one = 6;
+const two = 6;
+const three = 10;
+
+if ( one === two || two !== three ) { console.log ( " Iron Man Rules " );}
+else { console.log ( "Batman Rules" );}
+if ( a + b === b + c ) { console.log ( "I hate Marvel Universe");}
+
